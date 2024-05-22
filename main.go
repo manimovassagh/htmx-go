746883cf39095cc330b3b6276455bc8709353436
Mani Movassagh <mani.mgh@gmail.com>
2024-05-22 23:06:58 +0200

    handle

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("start point")
+	app := echo.New()
+	app.GET("/", HomeHandler)
+	app.Logger.Fatal(app.Start(":4000"))
+}
+
+// This custom Render replaces Echo's echo.Context.Render() with templ's templ.Component.Render().
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	buf := templ.GetBuffer()
+	defer templ.ReleaseBuffer(buf)
+
+	if err := t.Render(ctx.Request().Context(), buf); err != nil {
+		return err
+	}
+
+	return ctx.HTML(statusCode, buf.String())
+}
+
+func HomeHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, Home())
 }
